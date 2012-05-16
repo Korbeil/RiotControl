bf59c239daf58a591c55126a8fb9fd01b1a88774
LEDUC <b.leduc.web@gmail.com>
2012-05-16 15:05:04 +0200

    return to virgin SummonerGames View (so many comits for nothing, i know >.<)

--- Web/Script/Module/SummonerGames.js
+++ Web/Script/Module/SummonerGames.js
@@ -16,65 +16,6 @@ function renderSummonerGames(summoner, games)
 {
 	var element = Builder.node('div', { id:'games' });
 	$('content').appendChild(element);
-
-	for( var i = 0; i < games.length; i = i + 2) {
-		renderSummonerGame( games[i] , i);
-		renderSummonerGame( games[i+1], i);
-	}
-}
- 
-function renderSummonerGame( game, div) {
-	var current = 'game_'+div;
-	$('games').insert( Builder.node( 'div', { id:current}));
-	
-	renderChampionTable( game, current);
 }
 
-function renderChampionTable( game, current) {
-	var championName = getChampionName(game.ChampionId);
 
-	var gameTable =
-		Builder.node( 'table',	{
-									width: '144px',
-									height: '168px',
-									cellpadding: '2',
-									cellspacing: '0',
-									display: 'inline-table',
-								});
-
-	var gameTable_tbody =
-		Builder.node( 'tbody');
-	
-	var gameTable_tr_championIcon =
-		Builder.node( 'tr', { colspan: '2'});
-	
-	var gameTable_tr_otherInformations =
-		Builder.node( 'tr');
-	
-	var gameTable_tr_otherInformations_td = Builder.node('td', { width: '52px'});
-	gameTable_tr_otherInformations_td = appendChilds( gameTable_tr_otherInformations_td,
-		Builder.node( 'img', 	{
-									src: getURL('Image/Spell/Small/' + game.SummonerSpell1 + '.png'),
-									alt: getSummonerSpell(game.SummonerSpell1),
-									style: 'width: 24px; height: 24px;',
-								}),
-		Builder.node( 'img', 	{
-									src: getURL('Image/Spell/Small/' + game.SummonerSpell2 + '.png'),
-									alt: getSummonerSpell(game.SummonerSpell2),
-									style: 'width: 24px; height: 24px;',
-								})
-	);
-	
-	gameTable_tr_championIcon = appendChilds( gameTable_tr_championIcon,
-		Builder.node('td', { colspan: '2'}, image('Champion/Large/' + championName + '.png', championName, 140, 140)));
-	gameTable_tr_otherInformations = appendChilds( gameTable_tr_otherInformations,
-		Builder.node('td', championName),
-		gameTable_tr_otherInformations_td
-	);
-	
-	gameTable_tbody.appendChild(gameTable_tr_championIcon);
-	gameTable_tbody.appendChild(gameTable_tr_otherInformations);
-	gameTable.appendChild(gameTable_tbody);
-	
-	$(current).insert(gameTable);
-}
