1fe0bb259254458e91487ac28695b040d3880573
LEDUC Baptiste <b.leduc.web@gmail.com>
2012-05-29 21:02:56 +0200

    modify website tree

---
+++ Website/include/function.php
@@ -0,0 +1,29 @@
+<?php
+
+  function connect() {
+	$config = parse_ini_file( 'config/config.ini', true);
+    $link = mysql_connect( $config['mysql']['hostname'], $config['mysql']['username'], $config['mysql']['password']);
+    mysql_select_db( $config['mysql']['database'], $link);
+    return $link;
+  }
+
+  function getSummoners( $auto_update = true) {
+    global $link;
+
+    if( $auto_update) {
+      $sql = "SELECT * FROM summoner WHERE update_automatically = 1";
+    } else {
+      $sql = "SELECT * FROM summoner WHERE update_automatically = 0";
+    }
+    $data = mysql_query( $sql, $link);
+    $array = Array();
+
+    while( $current = mysql_fetch_array( $data)) {
+      $array[] = $current;
+    }
+
+    return $array;
+  }
+
+?>
+
