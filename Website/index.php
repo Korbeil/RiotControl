b25230b9fa5c1a3a697b5aad22d1c539202ea405
LEDUC <b.leduc.web@gmail.com>
2012-06-01 11:23:37 +0200

    modify Home style

--- Website/index.php
+++ Website/index.php
@@ -2,12 +2,35 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8">
-	</head>
+	  <script src="http://script.aculo.us/prototype.js" type="text/javascript"></script>
+    <script src="http://script.aculo.us/scriptaculous.js" type="text/javascript"></script>
+  </head>
 	<body>
-		<table width="100%"><tr width="100%"><td valign="center"><center>
-			<h3>RiotControl Tools</h3><br/><br/>
-			
-			<a href="SummonerList.php">Summoner List</a>
-		</center></td></tr></table>
+    <style>
+      #menu {
+        width:100%;
+        height:40px;
+        border-bottom:2px solid #000000;
+        background-color:white;
+        position: fixed;
+        top:0px;
+        left:0px;
+      }
+
+      #content {
+        border:1px solid #000000;
+      }
+    </style>
+
+    <div id="content">
+      <div id="menu">
+        <center><h3>RiotControl Tools</h3></center>
+      </div>
+
+      <table width="100%"><tr width="100%"><td valign="center"><center>
+        <br/><br/>
+        <a href="SummonerList.php">Summoner List</a>
+		  </center></td></tr></table>
+    </div>
 	</body>
 </html>
