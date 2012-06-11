78e3d2a4febba830be0cb55276b8ca01b92732f4
LEDUC B <b.leduc.web@gmail.com>
2012-06-11 16:19:14 +0200

    test new combo box UI

--- Website/include/page/footer.php
+++ Website/include/page/footer.php
@@ -1,4 +1,9 @@
 		<script src="js/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		<script>
+			$(document).ready(function(){
+				$('.combobox').combobox();
+			});
+		</script>
 	</body>
 </html>
