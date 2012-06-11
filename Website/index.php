1570a5e34727468dae91a4a4a8e0853a0d90b332
LEDUC <b.leduc.web@gmail.com>
2012-06-11 11:04:53 +0200

    Some tests (very beautifull UI), need structuration

--- Website/index.php
+++ Website/index.php
@@ -10,9 +10,31 @@
 ?>
 
 <div class="container">
+  <br/>
   <div class="well">
 
-
+  <div class="tabbable"> <!-- Only required for left/right tabs -->
+    <ul class="nav nav-tabs">
+      <li class="active"><a href="#home" data-toggle="tab">Home</a></li>
+      <li><a href="#items" data-toggle="tab">Items</a></li>
+      <li><a href="#champions" data-toggle="tab">Champions</a></li>
+      <li class="pull-right">
+        <div class="input-append">
+          <input type="search" class="span3" placeholder="Search" name="search" id="search"><a class="btn add-on" href="#"><i class="icon-search"></i></a>
+        </div>
+      </li>
+    </ul>
+    <div class="tab-content">
+      <div class="tab-pane active" id="home">
+        <p>Home pane</p>
+      </div>
+      <div class="tab-pane" id="items">
+        <p>Items pane</p>
+      </div>
+      <div class="tab-pane" id="champions">
+        <p>Champions pane</p>
+      </div>
+    </div>
   </div>
 </div>
 
