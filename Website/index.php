858bc85687b74bdd65eacf125ab7fa3a47f82e5c
LEDUC B <b.leduc.web@gmail.com>
2012-06-11 17:07:48 +0200

    new drop down button (test)

--- Website/index.php
+++ Website/index.php
@@ -20,11 +20,25 @@
       <li><a href="#summoner" data-toggle="tab">Summoners</a></li>
       <li><a href="#database" data-toggle="tab">Database</a></li>
       <li><a href="#about" data-toggle="tab">About</a></li>
+      
       <li class="pull-right">
         <div class="input-append">
           <input type="search" class="span3" placeholder="Search" name="search" id="search"><a class="btn add-on" href="#"><i class="icon-search"></i></a>
         </div>
       </li>
+      
+      <li class="pull-right">
+      	<div class="btn-group">
+    		<button class="btn" id="search-type">Summoner</button>
+        	<button class="btn dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>
+        	<ul class="dropdown-menu">
+        		<li><a href="#">Summoner</a></li>
+        		<li><a href="#">Champion</a></li>
+        		<li><a href="#">Item</a></li>
+          	</ul>
+        </div>
+      </li>
+      
     </ul>
     
     <!-- content -->
