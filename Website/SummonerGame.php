34186215a507c6540e4b2f33e25a6f7b68b4f5a3
LEDUC B <b.leduc.web@gmail.com>
2012-06-11 14:05:51 +0200

    first SummonerGameSummary preview (need more work, position and that f****ng huge whitespace at right)

---
+++ Website/SummonerGame.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+  </head>
+  <body>
+	<div class="container">
+		<div class="row">
+    		<div class="span6 well"> 
+        		<div class="row">
+        			<div class="span2">
+           				<img src="103.jpg" /><br /><br />
+          			</div>
+          			<div class="span1"><span class="badge badge-info">10</span><br /><br />RANKED 1789 ELO</div>
+          			<div class="span1"><span class="badge badge-important">5</span></div>
+          			<div class="span1"><span class="badge badge-success">12</span></div>
+        </div>
+        <div class="row">
+          <div class="span2"><img src="Flash.png" width="38" height="38"/> <img src="Ignite.png" width="38" height="38" /></div>
+          <div class="span3">June 7, 2012 14:10</div>
+          <div class="span1 offset4"><button type="button" class="btn pull-right">More</button></div>
+        </div>
+      </div>
+    </div>
+    <div class="span6"></div>
+  <div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
